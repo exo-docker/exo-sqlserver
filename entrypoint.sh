ea9a04f15f954c08cfc140442de82d11bc6293a0
Vincent Sellier <vsellier@exoplatform.com>
2017-06-01 18:12:37 +0200

    Log started when the database is really ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,13 +31,13 @@ cd /sqlserver
 # Wait for database availability
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -Q "select 1"
 
-echo "Database started"
+echo "Database configuration in progress..."
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -Q "create database ${SQLSERVER_DATABASE};"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -Q "create login ${SQLSERVER_USER} with password = '${SQLSERVER_PASSWORD}', DEFAULT_DATABASE=[${SQLSERVER_DATABASE}];"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -d "${SQLSERVER_DATABASE}" -Q "create user ${SQLSERVER_USER}"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -d "${SQLSERVER_DATABASE}"  -Q "EXEC sp_addrolemember N'db_owner', N'${SQLSERVER_USER}';"
 
-echo "Database initialized"
+echo "Database started"
 
 wait
